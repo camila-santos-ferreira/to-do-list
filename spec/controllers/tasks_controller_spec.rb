f8fd844ac0ac23857b165632f7f23bb8b313d951
Camila <camilaferreira68@hotmail.com>
2022-11-06 23:17:19 -0300

    testar método index da controller de tarefas

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  describe 'GET index' do
+    let!(:first_task) { create(:task) }
+    let!(:second_task) { create(:task) }
+
+    it 'assigns @tasks' do
+      get :index
+      expect(assigns(:tasks)).to eq([first_task, second_task])
+    end
+
+    it 'returns a 200 http status' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 end
