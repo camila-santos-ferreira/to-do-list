337469ce704cda9085ecf2136f711ae74c5c7d4c
Camila <camilaferreira68@hotmail.com>
2022-11-07 22:07:33 -0300

    criar método para buscar tarefa

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,12 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
 end
