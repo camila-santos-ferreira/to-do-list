747ed5622c03f7f9535ba6f0de357c821eb630b3
Camila <camilaferreira68@hotmail.com>
2022-11-10 00:35:16 -0300

    testar método que valida se o status da tarefa é pendente

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it { should validate_presence_of(:name) }
+
+  describe '#pending?' do
+    let!(:task) { create(:task) }
+
+    context 'when the task status is pending' do
+      it 'returns true' do
+        expect(task.pending?).to be(true)
+      end
+    end
+
+    context 'when the task status is not pending' do
+      before do
+        task.update_column(:status, :done)
+      end
+
+      it 'returns false' do
+        expect(task.pending?).to be(false)
+      end
+    end
+  end
 end
